8b6011d2a98effca51bca7d1d57a024640c3f4cb
Jason Carter <jasonwcarter@outlook.com>
2015-05-06 20:58:46 -0600

    Adds firing projectiles and different enemies

--- hello_world.rb
+++ hello_world.rb
@@ -1,23 +1,25 @@
 require 'gosu'
 
 module ZOrder
-  Background, Stars, Player, UI = *0..3
+  Background, Stars, Player, Projectile, UI = *0..4
 end
 
 # Main loop
 class GameWindow < Gosu::Window
   def initialize
     # height, width, fullscreen
-    super(640, 480, false)
+    super(1280, 960, false)
     self.caption = "Gosu Tutorial Game"
 
     @background_image = Gosu::Image.new(self, "assets/space.jpg", true)
 
     @player = Player.new(self)
-    @player.warp(320, 240)
+    @player.warp(640, 480)
 
-    @star_anim = Gosu::Image::load_tiles(self, "assets/coin.gif", 25, 25, false)
+    @star_anim = Gosu::Image::load_tiles(self, "assets/monster.png", 430, 385, false)
+
     @stars = Array.new
+    @projectiles = Array.new
 
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
   end
@@ -33,7 +35,10 @@ class GameWindow < Gosu::Window
       @player.accelerate
     end
     @player.move
-    @player.collect_stars(@stars)
+    @projectiles.each do |projectile| 
+      projectile.move
+      projectile.collect_stars(@stars)
+    end
 
     if rand(100) < 4 and @stars.size < 25 then
       @stars.push(Star.new(@star_anim))
@@ -44,23 +49,27 @@ class GameWindow < Gosu::Window
     @background_image.draw(0, 0, ZOrder::Background)
     @player.draw
     @stars.each { |star| star.draw }
-    @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+    @projectiles.each{ |projectile| projectile.draw }
+    @font.draw("Score: #{$score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
   end
 
   def button_down(id)
     if id == Gosu::KbEscape
       close
+    elsif id == Gosu::KbSpace
+      @projectiles.push(Projectile.new(self, @player.x, @player.y, @player.angle))
     end
   end
 end
 
 class Player
+  attr_reader :x, :y, :angle
   # Initializes the player object, takes a window where the image should be drawn
   def initialize(window)
     @image = Gosu::Image.new(window, "assets/mario.png", false)
     @beep = Gosu::Sample.new(window, "assets/coin_sound.wav")
     @x = @y = @vel_x = @vel_y = @angle = 0.0
-    @score = 0
+    $score = 0
   end
 
   def warp(x, y)
@@ -83,8 +92,8 @@ class Player
   def move
     @x += @vel_x
     @y += @vel_y
-    @x %= 640
-    @y %= 480
+    @x %= 1280
+    @y %= 960
 
     @vel_x *= 0.95
     @vel_y *= 0.95
@@ -95,19 +104,7 @@ class Player
   end
 
   def score
-    @score
-  end
-
-  def collect_stars(stars)
-    stars.reject! do |star|
-      if Gosu::distance(@x, @y, star.x, star.y) < 35 then
-        @score += 10
-        @beep.play
-        true
-      else
-        false
-      end
-    end
+  $score
   end
 end
 
@@ -116,18 +113,55 @@ class Star
 
   def initialize(animation)
     @animation = animation
-    @color = Gosu::Color.new(0xff000000)
-    @color.red = rand(256 - 40) + 40
-    @color.green = rand(256 - 40) + 40
-    @color.blue = rand(256 - 40) + 40
-    @x = rand * 640
-    @y = rand * 480
+    @x = rand * 1280
+    @y = rand * 960
   end
 
   def draw  
     img = @animation[Gosu::milliseconds / 100 % @animation.size];
     img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
-        ZOrder::Stars, 1, 1, @color, :add)
+        ZOrder::Stars, 1, 1)
+  end
+end
+
+class Projectile
+  def initialize(window, playerx, playery, playerangle)
+    @beep = Gosu::Sample.new(window, "assets/coin_sound.wav")
+    @image = Gosu::Image.new(window, ["assets/hadouken.png", "assets/hadouken-red.png"].sample)
+    @x = playerx
+    @y = playery
+    @angle = playerangle
+    @vel_x = 0
+    @vel_y = 0
+  end
+
+  def accelerate
+    @vel_x += Gosu::offset_x(@angle, 0.5)
+    @vel_y += Gosu::offset_y(@angle, 0.5)
+  end
+
+   def move
+    @x += @vel_x * 2
+    @y += @vel_y * 2
+    # @x %= 1280
+    # @y %= 960
+  end
+  
+  def draw
+    @image.draw_rot(@x, @y, ZOrder::Projectile, @angle)
+    self.accelerate
+  end 
+
+  def collect_stars(stars)
+    stars.reject! do |star|
+      if Gosu::distance(@x, @y, star.x, star.y) < 200 then
+        $score += 10
+        @beep.play
+        true
+      else
+        false
+      end
+    end
   end
 end
 
