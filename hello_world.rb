d084391f668b1d1fa3f9d941cb6edfd2434c23fc
Ben Eggett <beneggett@gmail.com>
2015-05-08 10:13:13 -0600

    More awesomeness

--- hello_world.rb
+++ hello_world.rb
@@ -23,6 +23,7 @@ class GameWindow < Gosu::Window
 
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @bg_music = Gosu::Song.new(self, "assets/zelda.mp3").play(true)
+    $timer = 0
   end
 
   def update
@@ -40,9 +41,32 @@ class GameWindow < Gosu::Window
       projectile.move
       projectile.collect_stars(@stars)
     end
+    $timer += 1
+    
+    case 
+    when $timer < 500
+      size = 3
+    when $timer < 1000
+      size = 5
+    when $timer < 2000
+      size = 10      
+    when $timer < 3000
+      size = 20   
+    when $timer < 4000
+      size = 40   
+    when $timer < 5000
+      size = 60
+    when $timer < 6000
+      size = 100
+    else
+      size = rand(100..10000)
+    end  
+    if rand(100) < 4 and @stars.size < size then
+      @stars.push(Star.new(@star_anim, self, @player))
+    end
 
-    if rand(100) < 4 and @stars.size < 25 then
-      @stars.push(Star.new(@star_anim))
+    if $score < 0 
+      self.close
     end
   end
 
@@ -52,6 +76,7 @@ class GameWindow < Gosu::Window
     @stars.each { |star| star.draw }
     @projectiles.each{ |projectile| projectile.draw }
     @font.draw("Score: #{$score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+    @font.draw("Level: #{$timer / 100.to_i}", 800, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
   end
 
   def button_down(id)
@@ -110,19 +135,52 @@ class Player
 end
 
 class Star
-  attr_reader :x, :y
+  attr_reader :x, :y, :window, :player
 
-  def initialize(animation)
+  def initialize(animation, window, player)
     @animation = animation
     @x = rand * 1280
     @y = rand * 960
+    @player = player
+    @window = window
+    
   end
 
   def draw  
     img = @animation[Gosu::milliseconds / 100 % @animation.size];
     img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
         ZOrder::Stars, 1, 1)
+    attack_player
   end
+
+
+  def attack_player
+    if Gosu::distance(@x, @y, @player.x, @player.y) < 200 then
+      dmg = 1
+      case 
+      when $timer < 200
+        dmg +=1
+      when $timer < 500
+        dmg +=2
+      when $timer < 1000
+        dmg +=3
+      when $timer < 2000
+        dmg +=4
+      when $timer < 3000
+        dmg +=8
+      when $timer < 5000
+        dmg +=15
+      else
+        dmg += 100
+      end        
+      $score -= dmg
+      @punch = Gosu::Sample.new(window, "assets/punch.mp3").play
+      true
+    else
+      false
+    end
+  end
+
 end
 
 class Projectile
